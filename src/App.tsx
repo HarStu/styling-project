699a818caeb90f11b241e3a686bb480704567e27
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-03 11:30:45 -0400

    Implement clickable check

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,23 @@ type TaskData = {
   id: number,
   title: String
   description: String
-  checked: Boolean
+  initialCheck: Boolean
 }
 
 type TaskProps = {
   taskData: TaskData
 }
-function Task({taskData: {title, description, checked}}: TaskProps) {
+function Task({taskData: {title, description, initialCheck}}: TaskProps) {
+  const [check, setCheck] = useState(initialCheck)
+
+  const toggleCheck = () => setCheck(!check);
   
   const baseBgStyle = "flex items-center pl-2 flex-row outline outline-gray-300 rounded-md w-100 py-3"
   const baseBtStyle = "flex outline outline-gray-300 w-5 h-5 mx-2 rounded-md"
 
   return (
-    <div className={clsx(baseBgStyle, checked && " bg-bgGreen")}>
-      <span className={clsx(baseBtStyle, checked && " bg-selectGreen")} />
+    <div className={clsx(baseBgStyle, check && " bg-bgGreen")}>
+      <span onClick={toggleCheck} className={clsx(baseBtStyle, check && " bg-selectGreen")} />
       <span className="font-[Inter] font-light space-y-1 mx-1.5">
         <p className="text-s">{title}</p>
         <p className="text-gray-400 text-xs">{description}</p>
@@ -30,7 +33,6 @@ function Task({taskData: {title, description, checked}}: TaskProps) {
 }
 
 function App() {
-  const [checked, setChecked] = useState(true)
 
   const taskData = {
     id: 1,
